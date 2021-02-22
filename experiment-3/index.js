424026d835c0f505949ead137cbf2843ad1da0b6
Roshan Sharma <reprogram46@gmail.com>
2021-02-22 21:35:21 +0530

    experiment-3: Reconstruct quantized signal
    Plot input & quantized samples too.

--- experiment-3/index.js
+++ experiment-3/index.js
@@ -1,5 +1,5 @@
 const plotlib = require('nodeplotlib')
-const { readAudio, quantize } = require('../utils')
+const { createWavefile, readAudio, quantize } = require('../utils')
 
 const FILENAME = 'samples/sample-1.wav'
 const LEVELS = 16
@@ -19,13 +19,45 @@ const quantizationErrors = [...Array(N)].map((_, idx) => {
   return Math.abs(originalSamples[idx] - quantizedSamples[idx])
 })
 
+// Plot input signal
+plotlib.plot(
+  [
+    {
+      x: [...Array(N).keys()],
+      y: originalSamples,
+      type: 'line',
+    },
+  ],
+  {
+    title: 'INPUT SIGNAL',
+    xaxis: { title: 'Time -->' },
+    yaxis: { title: 'Amplitude -->' },
+  },
+)
+
+// Plot quantized signal
+plotlib.plot(
+  [
+    {
+      x: [...Array(N).keys()],
+      y: quantizedSamples,
+      type: 'line',
+    },
+  ],
+  {
+    title: 'QUANTIZED SIGNAL',
+    xaxis: { title: 'Time -->' },
+    yaxis: { title: 'Amplitude -->' },
+  },
+)
+
 // Plot quantization errors
 plotlib.plot(
   [
     {
       x: [...Array(N).keys()],
       y: quantizationErrors,
-      type: 'bar',
+      type: 'line',
     },
   ],
   {
@@ -34,3 +66,8 @@ plotlib.plot(
     yaxis: { title: 'Quantization Error -->' },
   },
 )
+
+// Reconstruct the signal
+
+const FS = 8000 // set to frequency used to record
+createWavefile(quantizedSamples, 8000, 'samples/1-quantized.wav')
